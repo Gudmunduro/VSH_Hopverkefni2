c1f1f18a4c7fdc0ae9d22e9dea8781ef60785be4
Gudmunduro <goh140400@gmail.com>
2016-11-24 08:58:06 +0000

    Síða

---
+++ include/script.js
@@ -0,0 +1,35 @@
+var currentPage = 1, pageCount = 3, disabled = false;
+
+
+function onScroll(e)
+{
+    if (disabled) return;
+    if (e.wheelDelta > 0)
+    {
+        lastPage();
+    }
+    else
+    {
+        nextPage();
+    }
+}
+
+function nextPage()
+{
+    if (currentPage == pageCount) return;
+    disabled = true;
+    document.getElementById("page" + currentPage).className = "lastPage";
+    currentPage++;
+    document.getElementById("page" + currentPage).className = "page";
+    setTimeout(function () { disabled = false; }, 400);
+}
+
+function lastPage()
+{
+    if (currentPage == 1) return;
+    disabled = true;
+    document.getElementById("page" + currentPage).className = "nextPage";
+    currentPage--;
+    document.getElementById("page" + currentPage).className = "page";
+    setTimeout(function () { disabled = false; }, 400);
+}
